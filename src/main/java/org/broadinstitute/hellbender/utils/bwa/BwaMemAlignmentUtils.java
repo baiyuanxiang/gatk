e9ea67d3f6760b5525b04f8c83802cacb6100e7b
vruano <valentin.ruano@gmail.com>
2017-08-18 04:41:40 -0400

    Some more testing but fails due to a bug in BwaMemAlignmentsUtils

--- src/main/java/org/broadinstitute/hellbender/utils/bwa/BwaMemAlignmentUtils.java
+++ src/main/java/org/broadinstitute/hellbender/utils/bwa/BwaMemAlignmentUtils.java
@@ -6,6 +6,7 @@ import org.broadinstitute.hellbender.tools.spark.sv.utils.SVUtils;
 import org.broadinstitute.hellbender.utils.BaseUtils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Utils to move data from a BwaMemAlignment into a GATKRead, or into a SAM tag.
@@ -87,6 +88,9 @@ public class BwaMemAlignmentUtils {
             samRecord.setBaseQualities(SAMRecord.NULL_QUALS);
         }
         if ( readGroup != null ) samRecord.setAttribute(SAMTag.RG.name(), readGroup);
+        if (samRecord.isValid() != null) {
+            throw new IllegalStateException(samRecord.isValid().stream().map(s -> s.getMessage()).collect(Collectors.joining(", ")));
+        }
         return samRecord;
     }
 
